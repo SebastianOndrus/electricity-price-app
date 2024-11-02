d92fa6215bb0a3c7c4dfa0c6d6281b939e9027dd
Sebastián Ondruš <sebastian.ondrus2002@gmail.com>
2024-11-02 21:53:56 +0100

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+import { Toaster } from '@/components/ui/toaster';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <Toaster />
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
